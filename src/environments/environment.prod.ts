323942c7a360dd5856769db6a2df05bdb8c56856
sachinmmane <tosachinmane@gmail.com>
2020-03-24 15:54:59 +0530

    Added Firebase ComponentDone following:1. Added Firebase configuration details.2. Installed Angular Firebase components (firebase@4.2.0 and angularfire2@4.0.0-rc.1)3. Importe the respective required modules (AngularFiremodule, AngularFireDatabaseModule, AngularFireAuthModule)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA3rjh2yyBtrzIPw38Y1jt_oTC3OR_Hjpo",
+    authDomain: "oshop-e28d7.firebaseapp.com",
+    databaseURL: "https://oshop-e28d7.firebaseio.com",
+    projectId: "oshop-e28d7",
+    storageBucket: "oshop-e28d7.appspot.com",
+    messagingSenderId: "894882610704",
+    appId: "1:894882610704:web:b6681675ae15c255729495"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA3rjh2yyBtrzIPw38Y1jt_oTC3OR_Hjpo",
+    authDomain: "oshop-e28d7.firebaseapp.com",
+    databaseURL: "https://oshop-e28d7.firebaseio.com",
+    projectId: "oshop-e28d7",
+    storageBucket: "oshop-e28d7.appspot.com",
+    messagingSenderId: "894882610704",
+    appId: "1:894882610704:web:b6681675ae15c255729495"
+  }
 };
