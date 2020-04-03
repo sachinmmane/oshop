0d8f1fb0bb4b21f002335173d0ee29f72549a243
sachinmmane <tosachinmane@gmail.com>
2020-04-03 16:37:32 +0530

    Implemented query parameter called return url

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,21 +1,20 @@
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router, CanActivate, RouterStateSnapshot } from '@angular/router';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthGuardService  implements CanActivate {
 
-  constructor(private auth: AuthService, private router:Router) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate(){
+  canActivate(route,  state : RouterStateSnapshot){
     // TODO :- Need to revise the code and improve knowledge about the map operator.
     return this.auth.user$.map(user=> {
       if (user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl : state.url }});
       return false;
-
     });
   }
 }
